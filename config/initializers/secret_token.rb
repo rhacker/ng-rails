fd195d31a90837f299282666bef336e25cfda543
nXqd <nxqd.inbox+rhacker@gmail.com>
2013-12-18 05:04:08 +0700

    inint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polling::Application.config.secret_key_base = '61ef00a0a2da6f42267aa41881a7a319245b31399c211fd9cda2796efd8cc189efb1d834518e8db90bd750bc33a341d18fd600cb4c5899f3e1dda8fa6cd0b983'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Polling::Application.routes.draw do
+  root to: 'home#index'
+end
